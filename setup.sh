fb3c77624a7bf5bf4082c98342150a38271db296
Tom Marren <thomasmarren@users.noreply.github.com>
2022-02-25 15:23:22 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env sh
+#
+# vim: textwidth=0:filetype=sh
+#
+
+# This is a rough outline of the steps that are necessary to setup a new system
+# with all my tools/settings on macos
+
+#######################################################
+#
+# Install Xcode from App Store or download link
+#
+#######################################################
+
+
+#######################################################
+#
+# Install homebrew if not found
+#
+#######################################################
+if [ ! $(which brew) ]; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+#######################################################
+#
+# Install navi
+# https://github.com/junegunn/fzf
+#
+#######################################################
+brew install fzf
+
+#######################################################
+#
+# Install navi
+# https://github.com/denisidoro/navi
+#
+#######################################################
+
+brew install navi
+echo '' >> ~/.zshrc
+echo '# navi' >> ~/.zshrc
+echo 'eval "$(navi widget zsh)"' >> ~/.zshrc
+echo 'export NAVI_PATH="$PATH/.navi_cheatsheets"' >> ~/.zshrc
+echo '#' >> ~/.zshrc
+mkdir -p ~/.navi_cheatsheets
+echo curl 'https://raw.githubusercontent.com/thomasmarren/dotfiles/main/.navi_cheatsheets/generic.cheat' > ~/.navi_cheatsheets/generic.cheat
